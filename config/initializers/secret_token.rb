246525e5d585045f6fe0b145032b456f578cff48
Dmitry Savitski <dmitry.savitski@altoros.com>
2013-03-19 16:15:32 +0300

    Added Rails project with pg gem.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubyFfiRegressionTest::Application.config.secret_token = 'fad5003b3e83bcbf4469f5f7cf446710ff953e16565a66882ad0c31b57002a3ec9d97243cc947ba0619572d7ffe3509c4ff88a443ce08a4b1bcd9cb2a489f36b'
